ab416503ab6398dbbf1a50826f1d80fa074ee1c6
Ivan De la O <91438010+ivandelao5@users.noreply.github.com>
2021-09-27 19:32:11 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +10,16 @@ def home():
 @app.route('/info')
 def info():
     return render_template('info.html')
+
+@app.route('/testform', methods=['POST'])
+def testform ():
+    number_1 = int(request.form['number1'])
+    number_2 = int(request.form['number2'])
+    result = sum((number_1,number_2))
+    print(result)
+    return render_template('paginap.html',resultado=result,textolibre="puto")
+
+
 
 
 if __name__ == '__main__':
