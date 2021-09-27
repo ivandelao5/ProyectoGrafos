e7a7e7ce9754a9cdbf481b73fa04c658af929793
Ivan De la O <91438010+ivandelao5@users.noreply.github.com>
2021-09-27 13:50:52 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('paginap.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
